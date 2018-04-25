69d982bb5bce423b6b3a616025bcdb255b874e0f
kissillesgergely <kissigergely@gmail.com>
2018-04-25 15:58:16 +0200

    Add project files.

---
+++ KinectFinal/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace KinectFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan / button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Steps / label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Serial / label2";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(164, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Open / button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Export / button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 212);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Initialize / button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Axis Distance / label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Object Height / label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Object Width / label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Base Height / label6";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(164, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(164, 167);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(481, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Received messages / label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(179, 319);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 347);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(128, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Sended message / label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(179, 347);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 519);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ KinectFinal/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Added namespaces
+using Microsoft.Kinect;                 //To use the essentail Kinect functionalities
+using System.Windows.Media.Media3D;     //To use the Point3D object
+using System.IO.Ports;                  //To be able to use the serial communication
+using System.Globalization;             //To adjust the decimal separation character
+
+namespace KinectFinal
+{
+    public partial class Form1 : Form
+    {
+        public List<Point3D> virtualPoints = new List<Point3D>();
+        public List<Point3D> rawPoints = new List<Point3D>();
+        public List<Point3D> cleanPoints = new List<Point3D>();
+
+        //The overall size of the physical object (approximatelly)
+        public double axisDistance;
+        public double modelWidth;
+        public double modelHeight;
+        public double baseHeight;
+
+        //The size of the object in pixels on the recorded picture
+        //These values are calculated from the physical parameters
+        //58.5 x 46.6 degrees resulting in an average of about 5 x 5 pixels per degree
+        public double sceneHeight;
+        public double sceneWidth;
+        public double sceneBase;
+
+        public int ppp;                 //pixels per points
+        public int scanlines;
+
+        public int remainingSteps;
+        public int Steps;
+        public double StepSize;           //this will be maped between 0 and 255, in correspndence to the 360 degrees
+        
+        public KinectSensor sensor;
+        public DepthImagePixel[] depthPixels;
+
+        //To replace ',' to '.' in float and double number (because of the Hungarian default)
+        NumberFormatInfo nfi = new NumberFormatInfo();
+
+        //---------------------------------------------//
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //---------------------------------------------//
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Find Kinect
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != sensor)
+            {
+                sensor.Start();
+                sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            }
+
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+            if (ports.Length>0) comboBox1.SelectedIndex = 0;
+            else MessageBox.Show("Please connect the turntable and restart!",
+                "No Turntable Found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+
+            button1.Enabled = false;
+            button3.Enabled = false;
+
+            serialPort1.ReadTimeout = 5000;
+
+        }
+
+        //---------------------------------------------//
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            scan();
+        }
+
+        //---------------------------------------------//
+
+        private void scan()
+        {
+            while (remainingSteps>0)
+            {
+                moveTable();
+                remainingSteps--;
+                Console.WriteLine(remainingSteps);
+            }
+            button4.Enabled = true;
+        }
+
+        //---------------------------------------------//
+
+        private void pickUpSurfacePoints()
+        {
+
+        }
+
+        //---------------------------------------------//
+
+        private void calculation()
+        {
+            //58.5 x 46.6 degrees resulting in an average of about 5 x 5 pixels per degree
+            //1.021 x 0.813 radians
+            double fullWidth = 2 * Math.Tan(1.021 / 2) * axisDistance;
+            double fullHeight = 2 * Math.Tan(0.813 / 2) * axisDistance;
+            sceneWidth = 640 * (modelWidth / fullWidth);
+            sceneHeight = 480 * (modelHeight / fullHeight);
+            sceneBase = 240 + 480 * (baseHeight / fullHeight);
+            StepSize = 255 / Steps;
+            Console.WriteLine("Step Size: "+StepSize.ToString());
+        }
+
+        //---------------------------------------------//
+
+        private void initilazeScan()
+        {
+            //Connect to the Arduino via serial port
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.Open();                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //Read the steps of the scan, to calculate the angle of each step
+            if (Convert.ToInt32(textBox1.Text) != 0)
+            {
+                Steps = Convert.ToInt32(textBox1.Text);
+            }
+            else Steps = 10;
+
+            //Remaining steps will be decreased during the scan, but initally equal with the steps
+            remainingSteps = Steps;
+
+            //Read the distance of the axis, to calculate the middlepoint of the scan
+            if (Convert.ToInt32(textBox2.Text) != 0)
+            {
+                axisDistance = Convert.ToInt32(textBox2.Text);
+            }
+            else axisDistance = 855;
+
+            //Read the height of the model, to filter the lower and higher parts of the picture
+            if (Convert.ToInt32(textBox3.Text) != 0)
+            {
+                modelHeight = Convert.ToInt32(textBox3.Text);
+            }
+            else modelHeight = 100;
+
+            //Read the width of the model, to filter the right and left parts of the picture
+            if (Convert.ToInt32(textBox4.Text) != 0)
+            {
+                modelWidth = Convert.ToInt32(textBox4.Text);
+            }
+            else modelWidth = 100;
+
+            //Read the base of the model, to define the start point of the filtering
+            if (Convert.ToInt32(textBox5.Text) != 0)
+            {
+                baseHeight = Convert.ToInt32(textBox5.Text);
+            }
+            else baseHeight = -60;
+
+        }
+
+        //---------------------------------------------//
+
+        private void moveTable()
+        {
+            if (serialPort1.IsOpen)
+            {
+                char m = (char)((int)(StepSize * (Steps - remainingSteps)));
+                Console.WriteLine((StepSize * (Steps - remainingSteps)).ToString());
+                string m2 = m.ToString();
+                //serialPort1.WriteLine("S"+ (StepSize * (Steps - remainingSteps)).ToString());
+                serialPort1.WriteLine("S" + m2);
+                //Console.WriteLine("S" + (StepSize * (Steps - remainingSteps)).ToString());
+                Console.WriteLine("S" + m2);
+                label10.Text = "S" + m2;
+                Console.WriteLine("answer: "+serialPort1.ReadLine());
+                Console.WriteLine("answer: "+serialPort1.ReadLine());
+            }
+        }
+
+        //---------------------------------------------//
+
+        private void exportData()
+        {
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter("polygon.ply", false))
+            {
+                file.WriteLine("ply");
+                file.WriteLine("format ascii 1.0");
+                file.WriteLine("comment This is your C# ply file");
+                file.WriteLine("element vertex " + Convert.ToString(virtualPoints.Count));
+                file.WriteLine("property float x");
+                file.WriteLine("property float y");
+                file.WriteLine("property float z");
+                file.WriteLine("end_header");
+                for (int i = 0; i < virtualPoints.Count; i++)
+                {
+                    file.WriteLine((virtualPoints[i].X / 1000).ToString(nfi) + " " + (virtualPoints[i].Y / 1000).ToString(nfi)
+                                    + " " + (virtualPoints[i].Z / 1000).ToString(nfi));
+                }
+            }
+        }
+
+        //---------------------------------------------//
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //---------------------------------------------//
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            initilazeScan();
+            calculation();
+            button1.Enabled = true;
+        }
+    }
+}
